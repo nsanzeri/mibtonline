ec431e6884173ed9fe6168145bf97f7914a01410
Alex Sanzeri <Alex Sanzeri@DESKTOP-PHHP86M>
2018-07-15 19:04:24 -0500

    -Rough Draft of Videos Page on portfolio-1-col.php -More Changes on Index.php

--- mibt-bootstrap/portfolio-1-col.php
+++ mibt-bootstrap/portfolio-1-col.php
@@ -96,13 +96,13 @@
       <div class="row">
         <div class="col-md-7">
           <a href="#">
-            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="https://i.ytimg.com/vi/axgf-mb9Ydw/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA16RkEtwxHoBWypncTIxRaQGDnnQ" alt="">
           </a>
         </div>
         <div class="col-md-5">
-          <h3>Project One</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-          <a class="btn btn-primary" href="#">View Project
+          <h3>Officially Speaking Vlog 51</h3>
+          <p>Potential targeting on the QB and what results, possible holding on scrimmage kick.</p>
+          <a class="btn btn-primary" href="#">View
             <span class="glyphicon glyphicon-chevron-right"></span>
           </a>
         </div>
@@ -115,13 +115,13 @@
       <div class="row">
         <div class="col-md-7">
           <a href="#">
-            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="https://i.ytimg.com/vi/KAUhFloinoE/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB20Ko1dAEMeWRyQkNpJBNb-4mOYg" alt="">
           </a>
         </div>
         <div class="col-md-5">
-          <h3>Project Two</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
-          <a class="btn btn-primary" href="#">View Project
+          <h3>Officially Speaking Vlog 50</h3>
+          <p>Post Scrimmage Kick enforcement and Targeting.</p>
+          <a class="btn btn-primary" href="#">View
             <span class="glyphicon glyphicon-chevron-right"></span>
           </a>
         </div>
@@ -134,13 +134,13 @@
       <div class="row">
         <div class="col-md-7">
           <a href="#">
-            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="https://i.ytimg.com/vi/ZVJURwIYTO4/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAsY3sIzPi7pj_fHD-gHYJ3dlofUg" alt="">
           </a>
         </div>
         <div class="col-md-5">
-          <h3>Project Three</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
-          <a class="btn btn-primary" href="#">View Project
+          <h3>MIBTonline Sample Association/Clinic Meeting</h3>
+          <p>Sample clip of the NEW online officiating association MIBTonline!</p>
+          <a class="btn btn-primary" href="#">View
             <span class="glyphicon glyphicon-chevron-right"></span>
           </a>
         </div>
@@ -178,13 +178,13 @@
           </a>
         </li>
         <li class="page-item">
-          <a class="page-link" href="#">1</a>
+          <a class="page-link" href="portfolio-1-col.php">1</a>
         </li>
         <li class="page-item">
-          <a class="page-link" href="#">2</a>
+          <a class="page-link" href="#">.</a>
         </li>
         <li class="page-item">
-          <a class="page-link" href="#">3</a>
+          <a class="page-link" href="#">.</a>
         </li>
         <li class="page-item">
           <a class="page-link" href="#" aria-label="Next">
