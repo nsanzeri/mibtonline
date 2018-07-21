d8102dcf1410d21eda47bf72f56f08af7dc8f93d
Alex Sanzeri <Alex Sanzeri@DESKTOP-PHHP86M>
2018-07-20 22:06:21 -0500

    -Start of Subscribe Page -Styling Change

--- mibt-bootstrap/blog-home-1.php
+++ mibt-bootstrap/blog-home-1.php
@@ -103,7 +103,12 @@
             <div class="card-body">
               <h2 class="card-title">Post Title</h2>
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="LDXFDDG5BEHS4">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+			</form>
             </div>
             <div class="card-footer text-muted">
               Posted on January 1, 2017 by
--- mibt-bootstrap/subscribe.php
+++ mibt-bootstrap/subscribe.php
@@ -27,26 +27,41 @@ require_once 'index.inc.php';
     </nav>
 
       <!-- Page Content -->
-    <div class="container">
-	    <div id="login">
-			<form action="php-includes/subscribe.inc.php" method="post" id="paypal_form" target="_blank">
-				<input type="hidden" name="cmd" value="_xclick" />
-				<input type="hidden" name="no_note" value="1" />
-				<input type="hidden" name="lc" value="UK" />
-				<input type="hidden" name="currency_code" value="GBP" />
-				<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_LG.gif:NonHostedGuest" />
-				<input type="hidden" name="first_name" value="Customer's First Name"  />
-				<input type="hidden" name="last_name" value="Customer's Last Name"  />
-				<input type="hidden" name="payer_email" value="customer@example.com"  />
-				<input type="hidden" name="item_number" value="123456" / >
-				<input type="submit" name="submit" value="Submit Payment"/>
-			</form>
-		</div>
-    </div>
-    <!-- /.container -->
-
-    <?php require_once 'footer.inc.php'; ?>
-
+         <div class="container">
+    
+          <!-- Page Heading/Breadcrumbs -->
+          <h1 class="mt-4 mb-3">Subscribe
+            <small>.</small>
+          </h1>
+    
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="index.html">Home</a>
+            </li>
+            <li class="breadcrumb-item active">Services</li>
+          </ol>
+    
+          <!-- Image Header -->
+          <img class="img-fluid rounded mb-4" src="http://placehold.it/1200x300" alt="">
+    
+          <!-- Marketing Icons Section -->
+          <div class="row">
+            <div class="container">
+              <div class="card h-100">
+                <h4 class="card-header">Live Assosciation</h4>
+                <div class="card-body">
+                  <p class="card-text">MIBT Online's new Live assosciation .</p>
+                </div>
+                <div class="card-footer">
+                  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick">             
+                    <input type="hidden" name="hosted_button_id" value="LDXFDDG5BEHS4">                    
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">                    
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">                   
+                    </form>
+                </div>
+              </div>
+            </div>
+          <!-- /.row -->       
   </body>
-
 </html>
