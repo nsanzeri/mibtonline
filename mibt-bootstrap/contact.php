ce9dd3708c5198170b27605cc9b1b9d844063a55
nsanzeri <nsanzeri@gmail.com>
2018-07-26 14:06:36 -0500

    New content for Clinic Include
    Contact Home button pointing to "index.php"

--- mibt-bootstrap/contact.php
+++ mibt-bootstrap/contact.php
@@ -36,7 +36,7 @@ require_once 'index.inc.php';
 
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
-          <a href="index.html">Home</a>
+          <a href="index.php">Home</a>
         </li>
         <li class="breadcrumb-item active">Contact</li>
       </ol>
--- mibt-bootstrap/php-includes/clinic.inc.php
+++ mibt-bootstrap/php-includes/clinic.inc.php
@@ -5,10 +5,6 @@ echo("Username".$_SESSION['login_user']);
   if (empty($_SESSION['login_user'])) {
       header( "Location: signup.php" ); die;
   } else {
-  	if(empty($_SESSION['is_paid'])) {
-  	    header( "Location: subscribe.php" ); die;
-  	}else{
-  		$content = "Live Video Coming Soon";
   	}
   }
 ?>
