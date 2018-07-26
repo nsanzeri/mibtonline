45f5f8e1cf61473ecb36bbdccc79f8de84956995
nsanzeri <nsanzeri@gmail.com>
2018-07-26 13:22:10 -0500

    Removing echo.

--- mibt-bootstrap/php-includes/live-training.inc.php
+++ mibt-bootstrap/php-includes/live-training.inc.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); // Starting Session
 $content=''; // Variable To Store Error Message
-echo("Username".$_SESSION['login_user']);
+// echo("Username".$_SESSION['login_user']);
   if (empty($_SESSION['login_user'])) {
       header( "Location: signup.php" ); die;
   } else {
