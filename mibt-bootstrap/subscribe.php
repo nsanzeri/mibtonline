574219ae754f3acffe30ce076fab486de3455859
Alex Sanzeri <Alex Sanzeri@DESKTOP-PHHP86M.attlocal.net>
2018-08-08 15:42:23 -0500

    subscribe button for redirect

--- mibt-bootstrap/subscribe.php
+++ mibt-bootstrap/subscribe.php
@@ -53,13 +53,21 @@ require_once 'index.inc.php';
                   <p class="card-text">MIBT Online's new Live assosciation will have many features such as live chats, state of the art live training, and much more. To subscribe please click the button below.</p>
                 </div>
                 <div class="card-footer">
-                  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                    <input type="hidden" name="cmd" value="_s-xclick">             
-                    <input type="hidden" name="hosted_button_id" value="LDXFDDG5BEHS4">                    
-                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">                    
-                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">                   
-                    </form>
-                </div>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post"
+							target="_top">
+
+							<input type="hidden" name="cmd" value="_s-xclick"> <input
+								type="hidden" name="hosted_button_id" value="LDXFDDG5BEHS4"> <input
+								type="image"
+								src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif"
+								border="0" name="submit"
+								alt="PayPal - The safer, easier way to pay online!"> <img alt=""
+								border="0"
+								src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+								width="1" height="1">
+
+						</form>
+					</div>
               </div>
             </div>
           <!-- /.row -->       
