ad34cd6f6bf32e98fcde5aabfb9c1e92c6841238
nsanzeri <nsanzeri@gmail.com>
2019-03-13 14:51:09 -0500

    Changing location of of CSS and JS files that are not found.  TODO: make sure rest of pages work

--- mibt-bootstrap/about.php
+++ mibt-bootstrap/about.php
@@ -24,6 +24,8 @@ require_once 'connect.inc.php';
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Custom styles for this template -->
     <link href="css/modern-business.css" rel="stylesheet">
--- mibt-bootstrap/index.php
+++ mibt-bootstrap/index.php
@@ -15,7 +15,10 @@ require_once 'index.inc.php';
 <html lang="en">
 
   <?php require_once 'header.inc.php'; ?>
-  
+      <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 
   <body>
 
